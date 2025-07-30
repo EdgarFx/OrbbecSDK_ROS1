d5d12defbfb72bde5b1a63d2e6384a8d00b08247
EdgarFx <edgarfei331@gmail.com>
2025-07-30 17:47:55 -0400

    modity SE3_transform_script

--- scripts/SE3_transform.py
+++ scripts/SE3_transform.py
@@ -9,15 +9,15 @@ d1_to_d3 = pp.SE3([-0.183869, -0.587545, 0.344056, -0.319156, 0.114013, 0.399843
 
 base_to_link1 = pp.SE3([0, 0, 0, 0, 0, 0, 1])
 
-d2_to_link2 = link2_to_d2.Inv()  # link2_to_d2的逆变换
-d3_to_link3 = link3_to_d3.Inv()  # link3_to_d3的逆变换
+d2_to_link2 = link2_to_d2.Inv()  # link2_to_d2 inv
+d3_to_link3 = link3_to_d3.Inv()  # link3_to_d3 inv
 
-# 计算base_to_link2
-# 变换链: base → link1 → d1 → d2 → link2
+# base_to_link2
+# base → link1 → d1 → d2 → link2
 base_to_link2 = base_to_link1 @ link1_to_d1 @ d1_to_d2 @ d2_to_link2
 
-# 计算base_to_link3  
-# 变换链: base → link1 → d1 → d3 → link3
+# base_to_link3  
+# base → link1 → d1 → d3 → link3
 base_to_link3 = base_to_link1 @ link1_to_d1 @ d1_to_d3 @ d3_to_link3
 
 print("base_to_link2:")
